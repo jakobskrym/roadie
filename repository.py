004e50b9a764d26ae5ead046b59f4b8535fb5166
jakobskrym <jakob@skrym.se>
2022-11-19 17:06:35 +0100

    more progress

--- repository.py
+++ repository.py
@@ -2,12 +2,20 @@ from utils import *
 from structs import *
 from parsing import *
 
+def generateRepositoryInsert(struct_name, table_name, initials, table_dict, table):
+
+    function_string = ""
+
+    return function_string
+
 def createRepositoryFile(table, package_name):
 
     out_file = ""
 
     table_name = f"{table['name'].strip()}"
     multi_name = f"{getMultiName(table_name)}"
+
+    table_dict = getTable(table['columns'])
 
     # Creating the main struct
     struct_name = table_name.title().replace("_","")
@@ -17,5 +25,7 @@ def createRepositoryFile(table, package_name):
     initials = re.sub('[^A-Z]', '', struct_name).lower()
 
     # Adding the repository functions
+    out_file += generateRepositoryInsert(struct_name, table_name, initials, table_dict, table)
+    #out_file += generateRepositoryRetrieval()
 
     return out_file
